0944a89bdd74ce489c97d72a29c06eda92b08997
yuanjiaojiao <18638520107@163.com>
2020-12-16 16:58:36 +0800

    杰瑞开始搬砖啦!!!

--- last-app/src/App.vue
+++ last-app/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <div>哈哈哈哈,我是杰瑞呀!</div>
     <div id="nav">
+
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
